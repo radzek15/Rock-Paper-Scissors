d66b86e89c70df2b937beafe34b40d960e314be4
radzek15 <radzek15@wp.pl>
2021-07-26 16:35:22 +0200

    re

--- game.py
+++ game.py
@@ -7,23 +7,18 @@ Massages = ['You win', 'You Lose', 'It is a tie']
 cp_choice = random.choice(options)
 choice = input('Please choose one of {}'.format(options))
 
-results = {
-    'rock':{
+results = {'rock':{
         'rock': Massages[2],
         'paper': Massages[0],
-        'scissors': Massages[1],
-    },
+        'scissors': Massages[1]},
     'paper' : {
         "rock": Massages[1],
         "paper": Massages[2], 
-        "scissors": Massages[0],
-    },
+        "scissors": Massages[0]},
     "scissors":{
         "rock": Massages[0],
         "paper": Massages[1],
-        "scissors": Massages[2],
-    }
-}
+        "scissors": Massages[2]}}
 
 try:
     Winner = results[cp_choice][choice]
