de4cdc65377a78fc84fee96f254c9b5b03b0a0b4
radzek15 <radzek15@wp.pl>
2021-07-26 16:56:42 +0200

    ending massage

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import random
-
+name = input('Enter your name: ')
 options = ['rock', 'paper', 'scissors']
 
 Massages = ['You win', 'You Lose', 'It is a tie']
@@ -7,22 +7,27 @@ Massages = ['You win', 'You Lose', 'It is a tie']
 cp_choice = random.choice(options)
 choice = input('Please choose one of {}'.format(options))
 
-results = {'rock':{
-        'rock': Massages[2],
-        'paper': Massages[0],
-        'scissors': Massages[1]},
-    'paper' : {
-        "rock": Massages[1],
-        "paper": Massages[2], 
-        "scissors": Massages[0]},
-    "scissors":{
-        "rock": Massages[0],
-        "paper": Massages[1],
-        "scissors": Massages[2]}}
+def win_or_lose(me,cp):
+    results = {'rock':{
+            'rock': Massages[2],
+            'paper': Massages[1],
+            'scissors': Massages[0]},
+        'paper' : {
+            "rock": Massages[0],
+            "paper": Massages[2], 
+            "scissors": Massages[1]},
+        "scissors":{
+            "rock": Massages[1],
+            "paper": Massages[0],
+            "scissors": Massages[2]}}
+
+    return results[me][cp]
 
 try:
-    Winner = results[cp_choice][choice]
+    Winner = win_or_lose(choice,cp_choice)
     print('Computer choose {}'.format(cp_choice))
     print(Winner)
 except KeyError:
     print('You choose wrong, Please try again')
+
+print('Thank you for playing {}. Play again anytime u want'.format(name))
