a0a1d74aa543a94c482eca7355e3cb067e3fb033
radzek15 <radzek15@wp.pl>
2021-07-26 14:14:19 +0200

    a

---
+++ game.py
@@ -0,0 +1,33 @@
+import random
+
+options = ['rock', 'paper', 'scissors']
+
+Massages = ['You win', 'You Lose', 'It is a tie']
+
+cp_choice = random.choice(options)
+choice = input('Please choose one of {}'.format(options))
+
+results = {
+    'rock':{
+        'rock': Massages[2],
+        'paper': Massages[0],
+        'scissors': Massages[1],
+    },
+    'paper' : {
+        "rock": Massages[1],
+        "paper": Massages[2], 
+        "scissors": Massages[0],
+    },
+    "scissors":{
+        "rock": Massages[0],
+        "paper": Massages[1],
+        "scissors": Massages[2],
+    }
+}
+
+try:
+    Winner = results[cp_choice][choice]
+    print('Computer choose {}'.format(cp_choice))
+    print(Winner)
+except KeyError:
+    print('You choose wrong, Please try again')
